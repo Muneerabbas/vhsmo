0c7c45c0d6c73b112b4d192a245b086a5cc56c8a
Muneerabbas <muneer.abbas9595@gmail.com>
2026-07-24 01:43:55 +0530

    date

--- src/lib/launch.ts
+++ src/lib/launch.ts
@@ -1,4 +1,4 @@
-export const LAUNCH_TIME = new Date("2026-07-22T18:00:00+05:30");
+export const LAUNCH_TIME = new Date("2026-07-26T18:00:00+05:30");
 
 export function hasLaunched() {
   return new Date() >= LAUNCH_TIME;
